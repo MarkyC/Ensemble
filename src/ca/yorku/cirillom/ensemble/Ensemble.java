bd85c593bf476b0792c40db2e67f96138f43039d
MarkyC <mmmarcooo@gmail.com>
2014-02-09 18:46:12 -0500

    Initial Commit

--- src/ca/yorku/cirillom/ensemble/Ensemble.java
+++ src/ca/yorku/cirillom/ensemble/Ensemble.java
@@ -6,4 +6,8 @@ package ca.yorku.cirillom.ensemble;
  * Date: 2/9/14 6:43 PM.
  */
 public class Ensemble {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
